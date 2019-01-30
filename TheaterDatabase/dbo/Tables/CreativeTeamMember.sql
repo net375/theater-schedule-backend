20bb56ada31eb5834e49497a79a8786c4d22c1c9
Kril Ostap <krilostap@gmail.com>
2019-01-30 18:52:58 +0200

    Added table PerformanceCreativeTeamMember

--- TheaterDatabase/dbo/Tables/CreativeTeamMember.sql
+++ TheaterDatabase/dbo/Tables/CreativeTeamMember.sql
@@ -1,6 +1,5 @@
 ﻿CREATE TABLE [dbo].[CreativeTeamMember] (
     [CreativeTeamMemberId] INT identity(1,1) NOT NULL,
-    [Profesion]        NVARCHAR (50) NOT NULL,
     [FistName]         NVARCHAR (50) NOT NULL,
     [LastName]         NVARCHAR (50) NOT NULL,
     CONSTRAINT [PK_CreativeTeamMember] PRIMARY KEY CLUSTERED ([CreativeTeamMemberId] ASC)
--- TheaterDatabase/dbo/Tables/Schedule.sql
+++ TheaterDatabase/dbo/Tables/Schedule.sql
@@ -4,6 +4,6 @@
     [Beginning]     DATETIME      NOT NULL,
     [PerformanceId] INT           NOT NULL,
     CONSTRAINT [PK_Performance] PRIMARY KEY CLUSTERED ([ScheduleId] ASC),
-    CONSTRAINT [FK_Performance_PerformanceDetails] FOREIGN KEY ([PerformanceId]) REFERENCES [dbo].[Performance] ([PerformanceId])
+    CONSTRAINT [FK_Schedule_Performance] FOREIGN KEY ([PerformanceId]) REFERENCES [dbo].[Performance] ([PerformanceId])
 );
 
