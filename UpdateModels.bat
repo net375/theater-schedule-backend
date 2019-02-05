d0bd41f2f01dc4d68db209603e4e48ba6a6092ff
SOFTSERVE\vtork <vtork@softserveinc.com>
2019-02-05 18:42:05 +0200

    Update ignore

--- UpdateModels.bat
+++ UpdateModels.bat
@@ -1,5 +1,5 @@
 cd TheaterSchedule
-copy appsettings.json D:\Repository\theater-schedule-backend\Entities
+copy appsettings.json ..\Entities
 cd ..\Entities
 dotnet ef dbcontext scaffold name=TheaterConnectionString Microsoft.EntityFrameworkCore.SqlServer -o Models -f --data-annotations -f
 del /f appsettings.json, appsettings.Development.json
