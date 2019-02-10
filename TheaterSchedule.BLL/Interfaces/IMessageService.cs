eb29914bfd99743c0b84d1d67a4d205646676e72
Vasyl Diakiv <vasyldyakiv@gmail.com>
2019-02-10 16:59:04 +0200

    added message endpoint and appropriate tables

---
+++ TheaterSchedule.BLL/Interfaces/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheaterSchedule.BLL.DTO;
+
+namespace TheaterSchedule.BLL.Interfaces
+{
+    public interface IMessageService
+    {
+        MessageDTO GetById(int id);
+        void SendMessage(MessageDTO newMessage);
+    }
+}
---
+++ TheaterSchedule/Controllers/MessageController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TheaterSchedule.BLL.DTO;
+using TheaterSchedule.BLL.Interfaces;
+
+namespace TheaterSchedule.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        IMessageService messageService;
+
+        public MessageController(IMessageService messageService)
+        {
+            this.messageService = messageService;
+        }
+
+        // GET: api/message/1
+        [HttpGet("{id}")]
+        public ActionResult<MessageDTO> GetMessage(int id)
+        {
+            MessageDTO message = messageService.GetById(id);
+
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return message;
+        }
+
+        // POST api/message
+        [HttpPost]
+        public ActionResult<MessageDTO> PostMessage([FromBody] MessageDTO message)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                messageService.SendMessage(message);
+                return StatusCode(201, message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+    }
+}
