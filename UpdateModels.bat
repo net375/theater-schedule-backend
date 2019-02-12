6075c963478127927e951da3f8fee10dc4850f45
Yura <yurapav99@gmail.com>
2019-02-12 19:09:04 +0200

    fix conflict

--- UpdateModels.bat
+++ UpdateModels.bat
@@ -1,7 +1,7 @@
 cd TheaterSchedule
 copy appsettings.json ..\Entities
 cd ..\Entities
-dotnet ef dbcontext scaffold  "Data Source=DESKTOP-MRJFJOT; Initial Catalog=TheaterDatabase;Trusted_Connection=True;"  Microsoft.EntityFrameworkCore.SqlServer -o Models -f --data-annotations -f
+dotnet ef dbcontext scaffold name=TheaterConnectionString Microsoft.EntityFrameworkCore.SqlServer -o Models -f --data-annotations -f
 del /f appsettings.json, appsettings.Development.json
 cd..
 pause
