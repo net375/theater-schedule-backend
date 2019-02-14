54ebd57c4ffe9a90f52cf6505d0f40e41265b102
Petro Yarish <YarishP17@gmail.com>
2019-02-14 12:24:42 +0200

    Tables and model created

---
+++ TheaterSchedule.DAL/Models/ExcursionDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheaterSchedule.DAL.Models
+{
+    class ExcursionDataModel
+    {
+        public string ExcursionName { get; set; }
+        public string ShortDescription { get; set; }
+        public string FullDescription { get; set; }
+        public byte[] Image { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
