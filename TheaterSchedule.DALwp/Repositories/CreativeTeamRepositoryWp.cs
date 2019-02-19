5a7bfe4be411f7920b8cfabe639f0c571307716d
Yura <46374790+yurapav99@users.noreply.github.com>
2019-02-19 17:58:38 +0200

    Yurapav99 lvnetzzz 67 investigage integration with site (#36)

---
+++ TheaterSchedule.DALwp/Repositories/CreativeTeamRepositoryWp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheaterSchedule.DAL.Interfaces;
+
+namespace TheaterSchedule.DALwp.Repositories
+{
+    class CreativeTeamRepositoryWp : ICreativeTeamRepository
+    {
+        //TODO
+        //No such fields in API: FistName, LastName. But they exist in site 
+    }
+}
---
+++ TheaterSchedule.DALwp/Repositories/TagRepositoryWp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheaterSchedule.DAL.Interfaces;
+using TheaterSchedule.DAL.Models;
+
+namespace TheaterSchedule.DALwp.Repositories
+{
+    class TagRepositoryWp : ITagRepository
+    {
+        //TODO
+    }
+}
