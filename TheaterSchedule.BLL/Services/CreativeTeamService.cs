fb24db7ab40e4920e81826a5a4cc7ad2e1cdc542
taras-tymchuk <tymakruk@gmail.com>
2019-03-03 15:42:43 +0200

    Added caching for creative team

--- TheaterSchedule.BLL/Services/CreativeTeamService.cs
+++ TheaterSchedule.BLL/Services/CreativeTeamService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using System.Collections.Generic;
+using Microsoft.Extensions.Caching.Memory;
 using TheaterSchedule.BLL.DTO;
 using TheaterSchedule.BLL.Interfaces;
 using TheaterSchedule.DAL.Interfaces;
@@ -11,24 +12,39 @@ namespace TheaterSchedule.BLL.Services
     {
         private ITheaterScheduleUnitOfWork theaterScheduleUnitOfWork;
         private ICreativeTeamRepository creativeTeamRepository;
+        private IMemoryCache memoryCache;
 
         public CreativeTeamService(
             ITheaterScheduleUnitOfWork theaterScheduleUnitOfWork,
-            ICreativeTeamRepository creativeTeamRepository )
+            ICreativeTeamRepository creativeTeamRepository,
+            IMemoryCache memoryCache )
         {
             this.theaterScheduleUnitOfWork = theaterScheduleUnitOfWork;
             this.creativeTeamRepository = creativeTeamRepository;
+            this.memoryCache = memoryCache;
         }
 
         public IEnumerable<TeamMemberDTO> LoadCreativeTeam(
             string languageCode, int performanceId )
         {
-            return new MapperConfiguration(
-                    cfg => cfg.CreateMap<TeamMember, TeamMemberDTO>() )
-                .CreateMapper()
-                .Map<IEnumerable<TeamMember>, IEnumerable<TeamMemberDTO>>(
-                    creativeTeamRepository.GetCreativeTeam(
-                        languageCode, performanceId ) );
+            var mapper = new MapperConfiguration(
+                cfg => cfg.CreateMap<TeamMember, TeamMemberDTO>() )
+                .CreateMapper();
+            IEnumerable<TeamMember> creativeTeam = null;
+            string cacheKey = GetCacheKey(languageCode, performanceId);
+
+            if (!memoryCache.TryGetValue(cacheKey, out creativeTeam))
+            {
+                creativeTeam = creativeTeamRepository.GetCreativeTeam(languageCode, performanceId);
+                memoryCache.Set(cacheKey, creativeTeam);
+            }
+
+            return mapper.Map<IEnumerable<TeamMember>, IEnumerable<TeamMemberDTO>>(creativeTeam);
+        }
+
+        private string GetCacheKey(string languageCode, int id)
+        {
+            return $"{languageCode}{id}";
         }
     }
 }
--- TheaterSchedule/Startup.cs
+++ TheaterSchedule/Startup.cs
@@ -1,4 +1,5 @@
-﻿﻿using Entities.Models;
+﻿﻿using System;
+using Entities.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -11,20 +12,21 @@ using TheaterSchedule.BLL.Interfaces;
 using TheaterSchedule.BLL.Services;
 using TheaterSchedule.DAL.Interfaces;
 using TheaterSchedule.DAL.Repositories;
- using TheaterSchedule.DALwp.Fake_Repositories;
- using TheaterSchedule.DALwp.Repositories;
+using TheaterSchedule.DALwp.Fake_Repositories;
+using TheaterSchedule.DALwp.Repositories;
 using TheaterSchedule.MiddlewareComponents;
 
 namespace TheaterSchedule
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        private const int expirationTimeInHours = 12;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -65,7 +67,8 @@ namespace TheaterSchedule
             services.AddScoped<IPromoActionService, PromoActionService>();
             services.AddScoped<IImageRepository, ImageRepository>();
             services.AddScoped<ICreativeTeamService, CreativeTeamService>();
-            services.AddMemoryCache();
+            services.AddMemoryCache(options => 
+                options.ExpirationScanFrequency = TimeSpan.FromHours(expirationTimeInHours));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
