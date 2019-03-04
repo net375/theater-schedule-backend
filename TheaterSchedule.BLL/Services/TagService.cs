1448679c35e0278cc76f767e65ef134c81c60064
dashakrashchenko <dashakrashenko@gmail.com>
2019-03-05 00:08:24 +0200

    Added caching

--- TheaterSchedule.BLL/Services/TagService.cs
+++ TheaterSchedule.BLL/Services/TagService.cs
@@ -4,21 +4,31 @@ using System.Text;
 using TheaterSchedule.BLL.DTO;
 using TheaterSchedule.BLL.Interfaces;
 using TheaterSchedule.DAL.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace TheaterSchedule.BLL.Services
 {
     public class TagService: ITagService
     {
         private ITagRepository tagRepository;
+        private IMemoryCache memoryCache;
+        public TagDTO tagRequest;
 
-        public TagService(ITagRepository tagRepository)
+
+        public TagService(ITagRepository tagRepository, IMemoryCache memoryCache)
         {
             this.tagRepository = tagRepository;
+            this.memoryCache = memoryCache;
         }
 
         public TagDTO LoadTagsById(int id)
         {
-            TagDTO tagRequest = new TagDTO() { TagName = tagRepository.GetTagsByPerformanceId(id).Result };
+            string memoryCacheKey = id.ToString();
+            if(!memoryCache.TryGetValue(memoryCache,out tagRequest))
+            {
+                tagRequest = new TagDTO() { TagName = tagRepository.GetTagsByPerformanceId(id).Result };
+                memoryCache.Set(memoryCacheKey, tagRequest);
+            }
             return tagRequest;
         }
     }
