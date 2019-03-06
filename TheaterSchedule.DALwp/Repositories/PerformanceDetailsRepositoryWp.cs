a6e7667adb28b6d45caec431be9679613df4e77f
Andrii Mudrak <amudrtc@training.local>
2019-03-06 20:25:00 +0200

    Changed to private submetods for WP

--- TheaterSchedule.DALwp/Repositories/PerformanceDetailsRepositoryWp.cs
+++ TheaterSchedule.DALwp/Repositories/PerformanceDetailsRepositoryWp.cs
@@ -11,87 +11,74 @@ using WordPressPCL;
 
 namespace TheaterSchedule.DALwp.Repositories
 {
-    public class RenderedItem: WordPressPCL.Models.Base
-    {
-        [JsonProperty( "rendered", DefaultValueHandling = DefaultValueHandling.Ignore )]
-        public string Rendered { get; set; }
-    }
-
-    public class AboutGroup : WordPressPCL.Models.Base
-    {
-    [JsonProperty("age", DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int Age { get; set; }
-
-    [JsonProperty("price", DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public string Price { get; set; }
-
-    [JsonProperty("duration", DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int Duration { get; set; }
-    }
-
-    public class ACF : WordPressPCL.Models.Base
-    {
-        [JsonProperty("about_group", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public AboutGroup AboutGroup { get; set; }
-    }
-
-    public class Performance: WordPressPCL.Models.Base
-    {
-        [JsonProperty( "title", DefaultValueHandling = DefaultValueHandling.Ignore )]
-        public RenderedItem Title { get; set; }
-
-        [JsonProperty( "content", DefaultValueHandling = DefaultValueHandling.Ignore )]
-        public RenderedItem Content { get; set; }
-
-        [JsonProperty( "featured_media", DefaultValueHandling = DefaultValueHandling.Ignore )]
-        public int Featured_media { get; set; }
-
-        [JsonProperty("acf", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public ACF AcfInfo { get; set; }
-
-
-    }
-
-
-    public class Media_detailsItem: WordPressPCL.Models.Base
-    {
-        [JsonProperty( "sizes", DefaultValueHandling = DefaultValueHandling.Ignore )]
-        public SizesItem Sizes { get; set; }
-    }
-
-    public class SizesItem: WordPressPCL.Models.Base
-    {
-        [JsonProperty( "large", DefaultValueHandling = DefaultValueHandling.Ignore )]
-        public ImageItem Large { get; set; }
-
-        [JsonProperty( "full", DefaultValueHandling = DefaultValueHandling.Ignore )]
-        public ImageItem Full { get; set; }
-    }
-
-    public class ImageItem: WordPressPCL.Models.Base
-    {
-        [JsonProperty( "source_url", DefaultValueHandling = DefaultValueHandling.Ignore )]
-        public string Source_url { get; set; }
-    }
-
-    public class Media: WordPressPCL.Models.Base
-    {
-        [JsonProperty( "media_details", DefaultValueHandling = DefaultValueHandling.Ignore )]
-        public Media_detailsItem Media_details { get; set; }
-    }
-
     public class PerformanceDetailsRepositoryWp: Repository, IPerformanceDetailsRepository
     {
-        public static async Task<Performance> GetPerformance( WordPressClient client, int perforamanceId )
+        private class RenderedItem : WordPressPCL.Models.Base
+        {
+            [JsonProperty("rendered", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public string Rendered { get; set; }
+        }
+        private class AboutGroup : WordPressPCL.Models.Base
+        {
+            [JsonProperty("age", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public int Age { get; set; }
+
+            [JsonProperty("price", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public string Price { get; set; }
+
+            [JsonProperty("duration", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public int Duration { get; set; }
+        }
+        private class ACF : WordPressPCL.Models.Base
+        {
+            [JsonProperty("about_group", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public AboutGroup AboutGroup { get; set; }
+        }
+        private class Performance : WordPressPCL.Models.Base
+        {
+            [JsonProperty("title", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public RenderedItem Title { get; set; }
+
+            [JsonProperty("content", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public RenderedItem Content { get; set; }
+
+            [JsonProperty("featured_media", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public int Featured_media { get; set; }
+
+            [JsonProperty("acf", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public ACF AcfInfo { get; set; }
+        }
+        private class Media_detailsItem : WordPressPCL.Models.Base
+        {
+            [JsonProperty("sizes", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public SizesItem Sizes { get; set; }
+        }
+        private class SizesItem : WordPressPCL.Models.Base
+        {
+            [JsonProperty("large", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public ImageItem Large { get; set; }
+
+            [JsonProperty("full", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public ImageItem Full { get; set; }
+        }
+        private class ImageItem : WordPressPCL.Models.Base
+        {
+            [JsonProperty("source_url", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public string Source_url { get; set; }
+        }
+        private class Media : WordPressPCL.Models.Base
+        {
+            [JsonProperty("media_details", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public Media_detailsItem Media_details { get; set; }
+        }
+        private static async Task<Performance> GetPerformance( WordPressClient client, int perforamanceId )
         {
             return await client.CustomRequest.Get<Performance>( $"wp/v2/performance/{perforamanceId}" );
         }
-
-        public static async Task<IEnumerable<Media>> GetMediaOfPerformance( WordPressClient client, int perforamanceId )
+        private static async Task<IEnumerable<Media>> GetMediaOfPerformance( WordPressClient client, int perforamanceId )
         {
             return await client.CustomRequest.Get<IEnumerable<Media>>( $"wp/v2/media?parent={perforamanceId}" );
         }
-
         public PerformanceDetailsDataModelBase GetInformationAboutPerformance( string phoneId, string languageCode, int perforamanceId )
         {
             var performance = GetPerformance( InitializeClient(), perforamanceId ).Result;
