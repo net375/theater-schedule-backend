1467e4788460cb83606518f43d5b57cb64ff5dee
Vasyl Diakiv <vasyldyakiv@gmail.com>
2019-03-26 10:36:06 +0200

    IsChecked changed

--- TheaterSchedule.DAL/Repositories/IsCheckedPerformanceRepository.cs
+++ TheaterSchedule.DAL/Repositories/IsCheckedPerformanceRepository.cs
@@ -19,14 +19,6 @@ namespace TheaterSchedule.DAL.Repositories
 
         public bool IsChecked( string phoneId, int performanceId)
         {
-            /*return (from performance in performances
-                    join wishlist in db.Wishlist on performance.PerformanceId equals wishlist.PerformanceId
-                    join account in db.Account on wishlist.AccountId equals account.AccountId
-                        into wishlist_join
-                    from w in wishlist_join.DefaultIfEmpty()
-                    where (w != null && w.PhoneIdentifier == phoneId &&
-                           (performance.PerformanceId) == performanceId)
-                    select w).Any();*/
             return (from account in db.Account
                     join wishlist in db.Wishlist on account.AccountId equals wishlist.AccountId
                     where account.PhoneIdentifier == phoneId && wishlist.PerformanceId == performanceId
