7b5620d9afc93478d784a5f1793c94b7e89615b1
andriy-mudrak <46355099+andriy-mudrak@users.noreply.github.com>
2019-03-27 13:45:17 +0200

    added swagger for PerformanceDetailsController (#68)

--- TheaterSchedule/Controllers/PerformanceDetailsController.cs
+++ TheaterSchedule/Controllers/PerformanceDetailsController.cs
@@ -17,19 +17,28 @@ namespace TheaterSchedule.Controllers
         {
             this.performanceDetailsService = performanceDetailsService;
         }
-
-        [HttpGet("{phoneId}/{languageCode}/GetInfo")]
+        /// <summary>
+        /// Loads details information about performance  from lvivpuppet.com
+        /// </summary>
+        /// <param name="phoneId"></param>
+        /// <param name="languageCode"></param>
+        /// <param name="performanceId"></param>
+        /// <returns>a details information about performance from lvivpuppet.com</returns>
+        /// <response code="200">Returns a details information about performance of appropriate language</response>
+        /// <response code="400">If url which you are sending is not valid</response>
+        /// <response code="404">If the information about performance is null</response> 
+        [HttpGet( "{phoneId}/{languageCode}/GetInfo/{performanceId}" )]
         [Produces("application/json")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(typeof(Exception), StatusCodes.Status400BadRequest)]
         public ActionResult<PerformanceDetailsBaseDTO> GetInfo(
-            string phoneId, string languageCode, int id)
+            string phoneId, string languageCode, int performanceId)
         {
             try
             {
                 PerformanceDetailsBaseDTO performanceDetails = performanceDetailsService
-                    .LoadPerformanceDetails(phoneId, languageCode, id);
+                    .LoadPerformanceDetails(phoneId, languageCode, performanceId );
 
                 if (performanceDetails == null)
                     return NotFound();
