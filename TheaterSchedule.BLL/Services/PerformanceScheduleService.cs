cdbcf70a08815d7beb9b5316f512f81518d45639
dashakrashchenko <dashakrashenko@gmail.com>
2019-03-27 17:06:47 +0200

    Added string formats

--- TheaterSchedule.BLL/Services/PerformanceScheduleService.cs
+++ TheaterSchedule.BLL/Services/PerformanceScheduleService.cs
@@ -19,7 +19,7 @@ namespace TheaterSchedule.BLL.Services
 
         public string GetMemoryCacheKey(int performanceId)
         {
-            return "Performance_" + performanceId.ToString();
+            return string.Format("Performance_" + performanceId.ToString());
         }
 
         public PerformanceScheduleDTO LoadScheduleData(int performanceId)
