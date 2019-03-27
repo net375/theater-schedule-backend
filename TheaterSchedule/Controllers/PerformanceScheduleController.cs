efa61d72008c304d44a7d1154c43c44c2132869b
dashakrashchenko <dashakrashenko@gmail.com>
2019-03-27 17:14:51 +0200

    Added swagger documentation to PerformanceSchedule controller (#71)

--- TheaterSchedule/Controllers/PerformanceScheduleController.cs
+++ TheaterSchedule/Controllers/PerformanceScheduleController.cs
@@ -16,6 +16,14 @@ namespace TheaterSchedule.Controllers
             this.performanceScheduleService = performanceScheduleService;
         }
 
+        /// <summary>
+        /// Loads particular performance schedule, main image, price, age and duration range from lvivpuppet.com
+        /// </summary>
+        /// <param name="id">particular performance id</param>
+        /// <returns>a schedule list, main image, price, age and duration range for appropriate performance</returns>
+        /// <response code="200">Returns a full info about appropriate performance</response>
+        /// <response code="400">If url which you are sending is not valid</response>
+        /// <response code="404">If the information about performance is null</response> 
         [HttpGet("{id}", Name = "Get")]
         [Produces("application/json")]
         [ProducesResponseType(StatusCodes.Status200OK)]
