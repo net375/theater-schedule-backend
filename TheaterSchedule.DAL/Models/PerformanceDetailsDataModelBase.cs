a7510858503931295467e7b6943609c2555a92f2
VolodyaKhydzik <parta142536@i.ua>
2019-07-08 18:02:24 +0300

    Resolve the problem of downloading prices of performances from WordPress

--- TheaterSchedule.DAL/Models/PerformanceDetailsDataModelBase.cs
+++ TheaterSchedule.DAL/Models/PerformanceDetailsDataModelBase.cs
@@ -3,6 +3,7 @@
     public class PerformanceDetailsDataModelBase
     {
         public int Duration { get; set; }
+        public int Price { get; set; }
         public int MinPrice { get; set; }
         public int MaxPrice { get; set; }
         public int MinimumAge { get; set; }
