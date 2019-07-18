931c471beace539970f0265b07604f0e2053e137
Iurii Antoniuk <ustasalexus9530@gmail.com>
2019-07-18 14:58:40 +0300

    Uncommented UseHttpsRedirection

--- TheaterSchedule/Startup.cs
+++ TheaterSchedule/Startup.cs
@@ -142,7 +142,7 @@ namespace TheaterSchedule
 
             //app.UseAuthentication();
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
 
             app.UseStaticFiles();
