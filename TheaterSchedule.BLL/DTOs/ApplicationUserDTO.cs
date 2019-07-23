360ade399590e011173d55daf4eddf5675a65d4e
Alex Danyuk <alexdanyuk@Alexs-MacBook-Pro.local>
2019-07-23 13:53:56 +0300

    Added fields validation

--- TheaterSchedule.BLL/DTOs/ApplicationUserDTO.cs
+++ TheaterSchedule.BLL/DTOs/ApplicationUserDTO.cs
@@ -5,31 +5,42 @@ namespace TheaterSchedule.BLL.DTOs
 {
     public class ApplicationUserDTO
     {
+        [Required]
         public int Id { get; set;}
+
         [Required(ErrorMessage = "You should enter your firstname")]
         public string FirstName { get; set; }
+
         [Required(ErrorMessage = "You should enter your lastname")]
         public string LastName { get; set; }
+
         [Required(ErrorMessage = "You should enter your birth date")]
         public string DateOfBirth { get; set; }
+
         [Required(ErrorMessage = "You should enter your city")]
-
         public string City { get; set; }
-        [Required(ErrorMessage = "You should enter your country")]
 
+        [Required(ErrorMessage = "You should enter your country")]
         public string Country { get; set; }
+
         [Required(ErrorMessage = "You should enter your email")]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required(ErrorMessage ="You should enter your password")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+        
         [Required]
         public string PhoneIdentifier { get; set; }
+
         [Required]
         public int? SettingsId { get; set; }
-        [Required(ErrorMessage = "You should enter your email address")]
-        [Phone]
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$", ErrorMessage = "Phone Number is not valid")]
+
         public string PhoneNumber { get; set; }
 
     }
