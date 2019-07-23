a8a548ebe0a69e5f74d066ddbfc650abc07b5a0b
Denys Shourek <shourekdenis@gmail.com>
2019-07-23 14:33:34 +0300

    Add validation in ChangeProfileModel

--- TheaterSchedule/Models/ChangeProfileModel.cs
+++ TheaterSchedule/Models/ChangeProfileModel.cs
@@ -4,29 +4,35 @@ namespace TheaterSchedule.Models
 {
     public class ChangeProfileModel
     {
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Id is required")]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "First Name is required")]
+        [DataType(DataType.Text)]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Last Name is required")]
+        [DataType(DataType.Text)]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Birth Date is required")]
         public string DateOfBirth { get; set; }
 
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "City is required")]
+        [DataType(DataType.Text)]
         public string City { get; set; }
 
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Country is required")]
+        [DataType(DataType.Text)]
         public string Country { get; set; }
 
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Email is required")]
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Phone Number is required")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$", ErrorMessage = "Phone Number is not valid")]
         public string PhoneNumber { get; set; }
     }
 }
